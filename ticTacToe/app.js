48dfdadb7ee4f06325afe3d200dc11acfcdf71b5
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-27 22:02:08 -0400

    initial folder nd file set up

---
+++ ticTacToe/app.js
@@ -0,0 +1,17 @@
+// Need a board => create a board
+let board = [
+  ['','',''],
+  ['','',''],
+  ['','','']
+];
+
+let player1 = "X";
+let player2 = "O";
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
